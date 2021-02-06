759bf91d0008390810f7428fdc5f824a4f9b3f7e
Yaroslav <jajaroslove@gmail.com>
2021-02-06 16:45:03 +0300

    My project all

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource AudioNotes;
+
+    [Header("Массив нот")]
+    [SerializeField] private AudioClip[] Notes = new AudioClip[7];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioNotes = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void PlaySound(int numberNotes)
+    {
+        AudioNotes.PlayOneShot(Notes[numberNotes]);
+    }
+}
